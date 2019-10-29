18f8e9ca179edb90144db979e7256825069297b9
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-29 02:05:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,10 +18,10 @@ def literal_aoh
   # held in the constants
   
   result = [
-    DON_G = { name=> "Don Gately", occupation=> "Live-in Staff" }
-    JOELLE_VD = { name=> "Joelle van Dyne", occupation=> "Radio Personality" }
-    PAT_M =  { name=> "Pat Monteseian", occupation=> "Staff" }
-    KATE_G = { name=> "Kate Gompert", occupation=> "None" }
+    DON_G = { name=> "Don Gately", occupation=> "Live-in Staff" },
+    JOELLE_VD = { name=> "Joelle van Dyne", occupation=> "Radio Personality" },
+    PAT_M =  { name=> "Pat Monteseian", occupation=> "Staff" },
+    KATE_G = { name=> "Kate Gompert", occupation=> "None" },
     BRUCE_G = { name=> "Bruce Green", occupation=> "Fan of Mildred" }
     ]
 
